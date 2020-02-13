22fd0c6595b60f7c793d9964006485fd829d9d87
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-02-13 09:11:21 +0900

    add batch

---
+++ src/batch4cron.sh
@@ -0,0 +1,7 @@
+/usr/local/bin/python 01_ItemGenerator.py
+/usr/local/bin/python 02_ManifestGenerator.py
+/usr/local/bin/python 11_PropertiesGenerator.py
+/usr/local/bin/python 12_ItemSetsGenerator.py
+/usr/local/bin/python 13_ResourceTemplatesGenerator.py
+/usr/local/bin/python 14_SitesGenerator.py
+/usr/local/bin/python 21_MediaGenerator.py
